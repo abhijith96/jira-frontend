4dc8a577886bd6993af0bfc8e3e3c46c86e84bc9
Milan Xaier <dragonclawxavier@gmail.com>
2018-06-19 14:52:46 +0530

    Added links to table

--- src/app/main/content/apps/todo-table/todo-table.component.ts
+++ src/app/main/content/apps/todo-table/todo-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-todo-table',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoTableComponent implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit() {
   }
+  // navigateTo(){
+  //     this.router.navigate(['/apps/todo'])
+  // }
 
 }
