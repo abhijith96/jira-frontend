3835634afc5c675ebeb61fae5667dcd9d6b02e0f
Milan Xaier <dragonclawxavier@gmail.com>
2018-06-21 10:16:05 +0530

    Fixing color scheme

--- src/app/main/content/apps/new-issue-type/new-issue-type.module.ts
+++ src/app/main/content/apps/new-issue-type/new-issue-type.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import {Routes, RouterModule } from '@angular/router';
+
 import { MatButtonModule, MatCheckboxModule, MatDatepickerModule, MatFormFieldModule, MatIconModule, MatInputModule, MatMenuModule, MatRippleModule, MatSelectModule, MatSidenavModule, MatGridList, MatGridListModule, MatList, MatListModule, MatListItem, MatCardModule, MatSelectionList } from '@angular/material';
+import { NgxDnDModule } from '@swimlane/ngx-dnd';
+
 import { FuseSharedModule } from '@fuse/shared.module';
+
 import { NewIssueTypeComponent } from './new-issue-type.component';
+// import { FuseAngularMaterialModule } from '../../components/angular-material/angular-material.module';
+// import { FuseCountdownModule, FuseHighlightModule, FuseMaterialColorPickerModule, FuseWidgetModule } from '@fuse/components';
+
 
 const routes : Routes = [
                           {
@@ -23,6 +30,13 @@ const routes : Routes = [
     MatInputModule,
     MatIconModule,
     MatCardModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatRippleModule,
+    // FuseAngularMaterialModule,
+    // FuseMaterialColorPickerModule,
+    NgxDnDModule
     
   ],
   declarations: [NewIssueTypeComponent],
